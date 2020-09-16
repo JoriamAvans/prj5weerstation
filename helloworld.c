d06e763f43ca30cd9c0587f07aa54f64ad6448cd
Xander de Keijzer <xander.dekeijzer@gmail.com>
2020-09-16 12:16:55 +0200

    Added test.c

--- helloworld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!");
-    printf("This is from Xander de Keijzer");
-    return 0;
-}
---
+++ nl.prj5weerstation.project/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "test/test.c"
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is from Xander de Keijzer\n");
+
+    test();
+    return 0;
+}
